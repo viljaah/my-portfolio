36c6e2e8dbfa8f8e85ed349c7fa4ef0daae96c4e
Vilja Henriksen <vilja.a.hen@gmail.com>
2026-04-16 12:16:42 +0200

    Add accessibility changes, alt text improvement

--- src/components/Skills/SkillsCard.jsx
+++ src/components/Skills/SkillsCard.jsx
@@ -10,7 +10,7 @@ function SkillsCard({ title, itemNames = [], item_images = [] }) {
       <div className='skills-container'>
         {itemNames.length > 0 && itemNames.map((itemName, index) => (
           <div key={index} className='skills-item'>
-            <img src={item_images[index]} alt={itemName}/>
+            <img src={item_images[index]} alt={`Skill ${itemName} icon`}/>
             <p>{itemName}</p>
           </div>
         ))}
